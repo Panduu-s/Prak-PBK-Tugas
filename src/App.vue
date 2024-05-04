d57a4af37e9bd94c71cad8480d5f449109b0e318
Panduu-s <pandusarma@student.uir.ac.id>
2024-05-04 21:57:59 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+
+<template>
+    <div id="app">
+    <p :class="{ 'highlight': isHighlighted }" :style="{ 'font-weight': isBold ? 'bold' : 'normal' }">Penggunaan Input Teks dengan class bindings dan style bindings</p>
+    <input type="text" v-model="inputText" @input="onInput">
+    <p>Input Teks: {{ inputText }}</p>
+  </div>
+  </template>
+
+<script setup>
+    import { ref } from 'vue'
+
+    const inputText = ref('');
+    const isHighlighted = ref(false);
+    const isBold = ref(false);
+    const Upercase = ref(false)
+
+    const onInput = () => {
+        isHighlighted.value = inputText.value.length > 5;
+        isBold.value = inputText.value.length > 10;
+    };
+</script>
+
+<style scoped>
+
+</style>
